dba8378889132798f82693fc237c53203d5a3f62
Ruslan <valuevruslan1999@gmail.com>
2020-01-19 07:55:57 +0200

    mushrooms

---
+++ Assets/Observer/Mushroom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool activated;
+    private float time = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!activated)
+        {
+            time = Time.time + 10;
+        }
+        if(time != 0 && Time.time > time)
+        {
+            activated = true;
+            time = 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            activated = false;
+    }
+}
---
+++ Assets/Observer/Observer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Mushroom mushroom1;
+    public Mushroom mushroom2;
+    public Boss1 boss;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mushroom1.activated && mushroom2.activated)
+        {
+            boss.disableShield = true;
+        }
+    }
+}
